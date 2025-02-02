3405ae473e698db3cfb26bc87b45aab1db982bd0
Aliipou <161300611+Aliipou@users.noreply.github.com>
2025-02-02 06:14:02 -0800

    Create script.js

---
+++ separated-on/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
